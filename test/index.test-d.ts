f1c4c9f903761fc0d6d5cf84eb264e93c0a91198
vhashimotoo <hashimoto.stream@gmail.com>
2021-06-09 13:31:07 +0300

    update tsd

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,5 +1,5 @@
 import { expectType } from 'tsd'
-import * as markdown from '../index'
+import markdown from '../index'
 
 expectType<Promise<string>>(markdown('something'))
 expectType<string>(await markdown('something'))
